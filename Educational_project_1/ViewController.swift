32e41213fcd1f3becacbfed15aa94911f961549b
SelfishMak <selfish.mak@gmail.com>
2021-11-15 21:38:53 +0300

    Initial Commit

---
+++ Educational_project_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Educational_project_1
+//
+//  Created by Владимир Макаров on 15.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
